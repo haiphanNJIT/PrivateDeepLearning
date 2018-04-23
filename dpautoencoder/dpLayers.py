0805deee5827bc550f76d7029240136bea2ca8ee
haiphanNJIT <phannhathai171185@gmail.com>
2018-04-22 22:07:35 -0400

    Add files via upload

---
+++ dpautoencoder/dpLayers.py
@@ -0,0 +1,302 @@
+'''
+Differentially Private Layers
+author: Hai Phan
+'''
+import numpy as np
+import tensorflow as tf
+import input_data
+from logisticRegression import LogisticRegression
+import math
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import candidate_sampling_ops
+from tensorflow.python.ops import embedding_ops
+from tensorflow.python.ops import gen_nn_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import sparse_ops
+from tensorflow.python.ops import variables
+
+class EncLayer(object):
+    '''Typical convolutional layer of MLP'''
+    def __init__(self, inpt, n_filter_in, n_filter_out, filter_size, W=None, b=None, activation=tf.nn.sigmoid):
+        '''
+        :param inpt: input for the layer, i.e., images
+        :param n_filter_in: the number of input feature maps
+        :param n_filter_out: the number of input feature maps
+        :param filter_size: the dimension of convolutional filter [filter_size, filter_size]
+        '''
+        # Initialize parameters: W and b
+        if W is None:
+            W = tf.Variable(tf.truncated_normal([filter_size, filter_size, n_filter_in, n_filter_out], stddev=0.1), dtype=tf.float32, name="W")
+        if b is None:
+            b = tf.Variable(tf.zeros([n_filter_out]), dtype=tf.float32, name="b")
+        self.W = W
+        self.b = b
+        self.input = inpt;
+        # the output
+        sum_W = tf.add(tf.nn.conv2d(inpt, self.W, strides=[1, 2, 2, 1], padding='SAME'), self.b)
+        self.output = activation(sum_W) if activation is not None else sum_W
+        # the input's shape
+        self.inputShape = inpt.get_shape().as_list()
+        # params of the layers
+        self.params = [self.W, self.b]
+            
+    # define the Chebyshev Polinomial approximation
+    def Chebyshev(self, x):
+        return (-5*x**7 + 21*x**5 - 35*x**3 + 35*x + 16)/(2.0**5) # L = 7
+    
+    def dpChebyshev(self, x, Delta, epsilon, batch_size):
+        coefficients = [-5.0, 21.0, -35.0, 35.0, 16.0] # L = 7
+        for i in range(0, len(coefficients)):
+            perturbFM = np.random.laplace(0.0, 1.0/(epsilon*batch_size), 1).astype(np.float32);
+            perturbFM = tf.multiply(perturbFM, Delta);
+            coefficients[i] += perturbFM;
+        return (tf.multiply(coefficients[0], x**7) + tf.multiply(coefficients[1], x**5) + tf.multiply(coefficients[2], x**3) + tf.multiply(coefficients[3], x**1) + coefficients[4])/(2.0**5)
+    
+    # sampling hidden neurons given visible neurons
+    def propup(self, v):
+        '''Compute the sigmoid activation for hidden units given visible units'''
+        h = tf.add(tf.nn.conv2d(v, self.W, strides=[1, 2, 2, 1], padding='SAME'), self.b)
+        # Get the max value of hidden neurons
+        max = tf.reduce_max(h)
+        # Normalization so that h will satisfy the Riemann integrable condition on [−1, 1]
+        h = h/max;
+        return tf.clip_by_value(self.Chebyshev(h), 0.0, 1.0) # values of hidden neurons must be bounded [0, 1]
+
+    # differentially private hidden terms given visible neurons
+    def dp_propup(self, v, Delta, epsilon, batch_size):
+        '''Compute the differentially private activation for hidden terms given visible units'''
+        h = tf.add(tf.nn.conv2d(v, self.W, strides=[1, 2, 2, 1], padding='SAME'), self.b)
+        max = tf.reduce_max(h)
+        h = h/max;
+        # hidden neurons have to be bounded in [0, 1] after the perturbation
+        Chebyshev_h = tf.clip_by_value(self.dpChebyshev(h, Delta, epsilon, batch_size), 0.0, 1.0)
+
+        return Chebyshev_h # return perturbed approximated polinomial coefficients h
+    
+    # transpose of convolutional RBM given hidden neurons
+    def decode(self, xShape, propup, activation=tf.nn.sigmoid):
+        rc_input = activation(tf.add(tf.nn.conv2d_transpose(propup, self.W,
+                    tf.stack([xShape, self.inputShape[1], self.inputShape[2], self.inputShape[3]]),
+                    strides=[1, 2, 2, 1], padding='SAME'), self.b))
+        return rc_input
+    
+    # reconstruct visible units from convolutional feature maps
+    def decode2(self, xShape, propup, activation=tf.nn.sigmoid):
+        # upsampling given hidden feature maps to obtain input's size feature maps. This step can be considered an actual deconvolution
+        upsample3 = tf.image.resize_images(propup, size=(self.inputShape[1],self.inputShape[2]), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        # reconstruct the original inputs
+        rc_input = activation(tf.nn.conv2d(input=upsample3, filter=tf.transpose(self.W, perm=[1, 0, 3, 2]), strides=[1, 1, 1, 1], padding='SAME'))
+        ###
+        return rc_input
+    
+    # get pre-training objective function, this is use for convolutional auto-encoder
+    def get_train_ops(self, xShape, learning_rate=0.1):
+        propup = self.propup(self.input)
+        rc_v = self.decode(xShape, propup, activation=tf.nn.sigmoid);
+        self.cost = tf.reduce_sum(tf.square(rc_v - self.input))
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.cost, var_list=self.params)
+        return optimizer
+    
+    # get differentially private pre-training energy function for convolutional RBM
+    def get_train_ops2(self, xShape, Delta, epsilon = 0.25, batch_size = 3600, learning_rate=0.1):
+        # compute h terms
+        propup = self.dp_propup(self.input, Delta, epsilon, batch_size)
+        # reconstruct v terms
+        rc_v = self.decode2(xShape, propup, activation=tf.nn.sigmoid);
+        # reconstruct h terms
+        rc_propup = self.dp_propup(rc_v, Delta, epsilon, batch_size)
+        # minimize the differentially private mean between the two energy functions: (1) contructed from the original input, (2) constructed from the reconstructed input. In other words, we use CD-1 to minimize the energy function.
+        self.cost = tf.reduce_mean(tf.abs(rc_propup - propup)) + tf.reduce_mean(tf.abs(rc_v - self.input))
+        # define AdamOptimizer optimization
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.cost, var_list=self.params)
+        return optimizer
+
+class ConvLayer(object):
+    '''Typical convolutional layer of MLP'''
+    def __init__(self, inpt, filter_size, n_in, n_out, W=None, b=None, activation=tf.nn.sigmoid):
+        '''
+        :param inpt: input for the layer, i.e., images
+        :param n_in: the number of input units
+        :param n_out: the number of output units
+        '''
+        if W is None:
+            W = tf.Variable(tf.truncated_normal([filter_size, filter_size, n_in, n_out], stddev=0.1), dtype=tf.float32, name="W")
+        if b is None:
+            b = tf.Variable(tf.zeros([n_out]), dtype=tf.float32, name="b")
+
+        self.W = W
+        self.b = b
+        # the output
+        sum_W = tf.add(tf.nn.conv2d(inpt, self.W, strides=[1, 2, 2, 1], padding='SAME'), self.b)
+        self.output = activation(sum_W) if activation is not None else sum_W
+        # params
+        self.params = [self.W, self.b]
+
+class ConvFlat(object):
+    '''Transforming layer from a convolutional layer to a fully connected layer'''
+    def __init__(self, inpt, xShape, n_out, activation=tf.nn.sigmoid):
+        '''
+        inpt: tf.Tensor, (one minibatch) [None, n_in]
+        xShape: the first dimention of the input, i.e., used to access the batch_size
+        n_out: int, number of output units
+        '''
+        # Initialize parameters W and b
+        shape = inpt.get_shape().as_list()
+        n_in = shape[1]*shape[2]*shape[3]
+        # weight
+        self.W = tf.Variable(tf.truncated_normal([n_in, n_out], stddev=0.1), dtype=tf.float32, name="W")
+        # bias
+        self.b = tf.Variable(tf.zeros([n_out]), dtype=tf.float32)
+        ###
+        # Compute the output
+        h = tf.reshape(inpt, tf.stack([xShape, n_in]))
+        sum_W = tf.matmul(h, self.W) + self.b
+        # Applying normalization for the flat connected layer
+        batch_mean2, batch_var2 = tf.nn.moments(sum_W,[0])
+        scale2 = tf.Variable(tf.ones([n_out]))
+        beta2 = tf.Variable(tf.zeros([n_out]))
+        BN_norm = tf.nn.batch_normalization(sum_W,batch_mean2,batch_var2,beta2,scale2,1e-3)
+        ###
+        self.output = activation(BN_norm) if activation is not None else BN_norm
+        #####
+        # keep track of variables
+        self.params = [self.W, self.b]
+
+class HiddenLayer(object):
+    '''Typical hidden layer of MLP'''
+    def __init__(self, inpt, n_in, n_out, activation=tf.nn.sigmoid):
+        '''
+        inpt: tf.Tensor, shape [n_examples, n_in]
+        n_in: int, the dimensionality of input
+        n_out: int, number of hidden units
+        W, b: tf.Tensor, weight and bias
+        activation: tf.op, activation function
+        '''
+        # weight
+        self.W = tf.Variable(tf.truncated_normal([n_in, n_out], stddev=0.1), dtype=tf.float32, name="W")
+        # bias
+        self.b = tf.Variable(tf.zeros([n_out]), dtype=tf.float32)
+        # shape
+        self.n_in = n_in;
+        self.n_out = n_out;
+        # the output
+        sum_W = tf.matmul(inpt, self.W) + self.b
+        # Applying normalization for the flat connected layer
+        batch_mean2, batch_var2 = tf.nn.moments(sum_W,[0])
+        scale2 = tf.Variable(tf.ones([n_out]))
+        beta2 = tf.Variable(tf.zeros([n_out]))
+        BN_norm = tf.nn.batch_normalization(sum_W,batch_mean2,batch_var2,beta2,scale2,1e-3)
+        ###
+        self.output = activation(BN_norm) if activation is not None else BN_norm
+        # params
+        self.params = [self.W, self.b]
+
+class Autoencoder(object):
+    '''Typical hidden layer of MLP'''
+    def __init__(self, inpt, n_in, n_out, activation=tf.nn.sigmoid):
+        '''
+        inpt: tf.Tensor, shape [n_examples, n_in]
+        n_in: int, the dimensionality of input
+        n_out: int, number of hidden units
+        W, b: tf.Tensor, weight and bias
+        activation: tf.op, activation function
+        '''
+        # weight
+        self.W = tf.Variable(tf.truncated_normal([n_in, n_out], stddev=0.1), dtype=tf.float32, name="W")
+        # bias
+        self.b = tf.Variable(tf.zeros([n_out]), dtype=tf.float32)
+        self.vbias = tf.Variable(tf.zeros([n_in]), dtype=tf.float32)
+        self.input = inpt;
+        # shape
+        self.n_in = n_in;
+        self.n_out = n_out;
+        # the output
+        sum_W = tf.matmul(inpt, self.W) + self.b
+        self.output = activation(sum_W) if activation is not None else sum_W
+        # params
+        self.params = [self.W, self.b, self.vbias]
+    
+    # compute hidden neurons given visible neurons
+    def propup(self, v):
+        '''Compute the sigmoid activation for hidden units given visible units'''
+        h = tf.matmul(v, self.W) + self.b;
+        return tf.sigmoid(h)
+    
+    # compute visible neurons given hidden neurons
+    def propdown(self, h):
+        """Compute the sigmoid activation for visible units given hidden units"""
+        return tf.nn.sigmoid(tf.matmul(h, tf.transpose(self.W)) + self.vbias)
+    
+    # get differentially private pre-training cross-entropy error function
+    def get_dp_train_ops(self, epsilon, data_size, learning_rate=0.1):
+        # compute Laplace noise injected into coefficients h
+        Delta = self.n_in*(self.n_out + 1/4 * self.n_out**2);
+        perturbFM = np.random.laplace(0.0, Delta/(epsilon*data_size), self.n_out)
+        perturbFM = np.reshape(perturbFM, [self.n_out]);
+        
+        # compute h terms
+        activation_h = self.propup(self.input) + perturbFM;
+        # reconstruct v terms
+        activation_v = self.propdown(activation_h)
+        
+        '''
+        Computes sigmoid cross entropy given `logits`, i.e., x = logits = activation_v, and z = self.input = labels.
+            
+        Measures the probability error in discrete classification tasks in which each
+        class, i.e., each visible neuron, is independent and not mutually exclusive.
+            
+        The logistic loss is
+        z * -log(sigmoid(x)) + (1 - z) * -log(1 - sigmoid(x))
+        = z * -log(1 / (1 + exp(-x))) + (1 - z) * -log(exp(-x) / (1 + exp(-x)))
+        = z * log(1 + exp(-x)) + (1 - z) * (-log(exp(-x)) + log(1 + exp(-x)))
+        = z * log(1 + exp(-x)) + (1 - z) * (x + log(1 + exp(-x))
+        = (1 - z) * x + log(1 + exp(-x))
+        = x - x * z + log(1 + exp(-x))
+            
+        For x < 0, to avoid overflow in exp(-x), we reformulate the above
+            
+        x - x * z + log(1 + exp(-x))
+        = log(exp(x)) - x * z + log(1 + exp(-x))
+        = - x * z + log(1 + exp(x))
+            
+        Hence, to ensure stability and avoid overflow, the implementation uses this
+        equivalent formulation
+            
+        max(x, 0) - x * z + log(1 + exp(-abs(x)))
+            
+        `logits` and `labels` must have the same type and shape. Let denote neg_abs_logits = -abs(activation_v) = -abs(W^T * activation_h). By Applying Taylor Expansion, we have:
+            
+        Taylor = max(activation_v, 0) - activation_v * self.input + log(1 + exp(-abs(activation_v)));
+            = max(activation_h * W^T, 0) -  self.input * (activation_h * W^T) + (math.log(2.0) + 0.5*neg_abs_logits + 1.0/8.0*neg_abs_logits**2)
+            = max(activation_h * W^T, 0) -  self.input * (activation_h * W^T) + (math.log(2.0) + 0.5*(-abs(activation_h * W^T)) + 1.0/8.0*(-abs(activation_h * W^T))**2)
+            
+        To ensure that Taylor is differentially private, we need to perturb all the coefficients, including the terms activation_h * W^T, self.input * (W^T * activation_h).
+            
+        Since 'self.input *' is an element-wise multiplication, activation_h can be considered coefficients of the term self.input * (W^T * activation_h). By applying Funtional Mechanism, we perturb self.input * (W^T * activation_h) as tf.matmul(activation_h + perturbFM, W^T) * self.input:
+            
+        activation_h += perturbFM; # activation_h = self.propup(self.input) + perturbFM; # (Lemma 2) where
+        
+        Delta = self.n_in*(self.n_out + 1/4 * self.n_out**2);
+        perturbFM = np.random.laplace(0.0, Delta/(epsilon*data_size), self.n_out)
+        perturbFM = np.reshape(perturbFM, [self.n_out]);
+            
+        To allow computing gradients at zero, we define custom versions of max and abs functions [Tensorflow].
+            
+        Source: https://github.com/tensorflow/tensorflow/blob/r1.4/tensorflow/python/ops/nn_impl.py @ TensorFlow
+        '''
+        
+        zeros = array_ops.zeros_like(activation_v, dtype=self.input.dtype)
+        cond = (activation_v >= zeros)
+        relu_logits = array_ops.where(cond, activation_v, zeros)
+        neg_abs_logits = array_ops.where(cond, -activation_v, activation_v)
+        #Taylor = math_ops.add(relu_logits - activation_v * self.input, math.log(2.0) + 0.5*neg_abs_logits + 1.0/8.0*neg_abs_logits**2)
+        self.cost = math_ops.add(relu_logits - activation_v * self.input, math.log(2.0) + 0.5*neg_abs_logits + 1.0/8.0*neg_abs_logits**2)
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(self.cost, var_list=self.params)
+        
+        return optimizer
+
+
---
+++ dpautoencoder/dpautoencoder.py
@@ -0,0 +1,176 @@
+'''
+Differentially Private Auto-Encoder
+author: Hai Phan
+'''
+import timeit
+import numpy as np
+import tensorflow as tf
+import input_data
+import math
+import os
+from logisticRegression import LogisticRegression
+from logisticRegression import dpLogisticRegression
+from dpLayers import HiddenLayer
+from dpLayers import Autoencoder
+from dpLayers import ConvFlat
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+class dpAutoEncoder(object):
+    '''
+    An implement of differentially private auto-encoder
+    '''
+    def __init__(self, n_in=784, n_out=10, hidden_layers_sizes=[25, 25], epsilon = 0.25, _batch_size = 280, finetuneLR = 0.01):
+        '''
+        :param n_in: int, the dimension of input
+        :param n_out: int, the dimension of output
+        :param hidden_layers_sizes: list or tuple, the number of hidden neurons, the last item will be the number of hidden neurons in the last hidden layer
+        :param epsilon: privacy budget epsilon
+        :param _batch_size: the batch size
+        :param finetuneLR: fine tunning learning rate
+        '''
+        # Number of layers
+        assert len(hidden_layers_sizes) > 0
+        self.n_layers = len(hidden_layers_sizes)
+        self.layers = []    # hidden layers
+        self.params = []       # keep track of params for training
+        self.last_n_in = hidden_layers_sizes[-1] # the number of hidden neurons in the last hidden layer
+        self.pretrain_ops = []; # list of pretrain objective functions for hidden layers
+        self.epsilon = epsilon; # privacy budget epsilon epsilon
+        self.batch_size = _batch_size; # batch size
+
+        # Define the input, output, Laplace noise for the output layer
+        self.x = tf.placeholder(tf.float32, shape=[None, n_in], name='x')
+        self.y = tf.placeholder(tf.float32, shape=[None, n_out])
+        self.LaplaceNoise = tf.placeholder(tf.float32, self.last_n_in);
+        ######
+        
+        #############################
+        ##Construct the Model########
+        #############################
+        # Create the 1st auto-encoder layer
+        Auto_Layer1 = Autoencoder(inpt=self.x, n_in = 784, n_out = hidden_layers_sizes[0], activation=tf.nn.sigmoid)
+        self.layers.append(Auto_Layer1)
+        self.params.extend(Auto_Layer1.params)
+        # get the pretrain objective function
+        self.pretrain_ops.append(Auto_Layer1.get_dp_train_ops(epsilon = self.epsilon, data_size = 50000, learning_rate= 0.01))
+        ###
+        
+        # Create the 2nd cauto-encoder layer
+        Auto_Layer2 = Autoencoder(inpt=self.layers[-1].output, n_in = self.layers[-1].n_out, n_out = hidden_layers_sizes[1], activation=tf.nn.sigmoid)
+        self.layers.append(Auto_Layer2)
+        self.params.extend(Auto_Layer2.params)
+        # get the pretrain objective function
+        self.pretrain_ops.append(Auto_Layer2.get_dp_train_ops(epsilon = self.epsilon, data_size = 50000, learning_rate= 0.01))
+        ###
+        
+        # Create the flat connected hidden layer
+        flat1 = HiddenLayer(inpt=self.layers[-1].output, n_in = self.layers[-1].n_out, n_out = self.last_n_in, activation=tf.nn.relu)
+        self.layers.append(flat1)
+        self.params.extend(flat1.params)
+        ###
+        
+        # Create the output layer
+        # We use the differentially private Logistic Regression (dpLogisticRegression) layer as the objective function
+        self.output_layer = dpLogisticRegression(inpt=self.layers[-1].output, n_in = self.last_n_in, n_out=n_out, LaplaceNoise = self.LaplaceNoise)
+        # We can also use the non-differentially private layer: LogisticRegression(inpt=self.layers[-1].output, n_in=hidden_layers_sizes[-1], n_out=n_out)
+        self.params.extend(self.output_layer.params)
+        ###
+
+        #######################################
+        ##Define Fine Tune Cost and Optimizer##
+        #######################################
+        # The finetuning cost
+        self.cost = self.output_layer.cost(self.y)
+        # train_op for finetuning with AdamOptimizer
+        global_step = tf.Variable(0, trainable=False)
+        #learning_rate = tf.train.exponential_decay(finetuneLR, global_step, 700, 0.96, staircase=True); # learning rate decay can be carefully used
+        # Fine tune with AdamOptimizer. Note that we do not fine tune the pre-trained parameters at the auto-encoder layers
+        self.train_op = tf.train.AdamOptimizer(finetuneLR).minimize(self.cost, var_list=[flat1.params, self.output_layer.params], global_step = global_step)
+        # The accuracy
+        self.accuracy = self.output_layer.accuarcy(self.y)
+        ###
+    
+    def generateNoise(n_in, epsilon, data_size, test = False):
+        Delta = 0.0;
+        if test == True: # do not inject noise in the test phase
+            Delta = 0.0;
+        else:
+            Delta = 10*(n_in + 1/4 * n_in**2); # global sensitivity for the output layer, note that 10 is the number of classes of the output layer
+        # Generate the Laplace noise
+        perturbFM = np.random.laplace(0.0, Delta/(epsilon*data_size), n_in)
+        perturbFM = np.reshape(perturbFM, [n_in]);
+        return perturbFM;
+
+    def pretrain(self, sess, X_train, batch_size=600, pretraining_epochs=10, lr=0.01,
+                    display_step=1):
+        '''
+        Pretrain the layers (just train the auto-encoder layers)
+        :param sess: tf.Session
+        :param X_train: the input of the train set (You might modify this function if you do not use the designed mnist)
+        :param batch_size: int
+        :param lr: float
+        :param pretraining_epoch: int
+        :param display_step: int
+        '''
+        print('Starting pretraining...\n')
+        start_time = timeit.default_timer()
+        batch_num = int(math.ceil(X_train.train.num_examples / batch_size)) # The number of batch per epoch
+        # Pretrain layer by layer
+        for i in range(self.n_layers-1):
+            # Get the cost of the current auto-encoder layer
+            cost = tf.reduce_mean(self.layers[i].cost);
+            # Get the objective function of the current auto-encoder layer
+            train_ops = self.pretrain_ops[i]
+            for epoch in range(pretraining_epochs):
+                avg_cost = 0.0
+                for j in range(batch_num):
+                    x_batch, _ = X_train.train.next_batch(batch_size)
+                    # training
+                    sess.run(train_ops, feed_dict={self.x: x_batch})
+                    # cost
+                    avg_cost += sess.run(cost, feed_dict={self.x: x_batch}) / batch_num
+                # print out the average cost every display_step
+                if epoch % display_step == 0:
+                    print("\tPretraing layer {0} Epoch {1} cost: {2}".format(i, epoch, avg_cost))
+
+        end_time = timeit.default_timer()
+        print("\nThe pretraining process ran for {0} minutes".format((end_time - start_time) / 60))
+    
+    def finetuning(self, sess, trainSet, training_epochs=2400, _epsilon = 0.25, _batch_size = 280, display_step=5):
+        '''
+        Finetuing the network
+        '''
+        print("\nStart finetuning...\n")
+        start_time = timeit.default_timer()
+        LapNoise = dpAutoEncoder.generateNoise(n_in = 25, epsilon = _epsilon, data_size = 50000, test = False); #Add Laplace noise in training
+        for epoch in range(training_epochs):
+            #avg_cost = 0.0
+            batch_num = int(math.ceil(trainSet.train.num_examples / _batch_size)) # The number of batch per epoch
+            for i in range(batch_num):
+                x_batch, y_batch = trainSet.train.next_batch(_batch_size)
+                # training
+                sess.run(self.train_op, feed_dict={self.x: x_batch, self.y: y_batch, self.LaplaceNoise: LapNoise})
+            # print out the average cost
+            if epoch % display_step == 0:
+                LapNoise_test = dpAutoEncoder.generateNoise(n_in = 25, epsilon = _epsilon, data_size = 50000, test = True); #Do not add noise when testing
+                val_acc = sess.run(self.accuracy, feed_dict={self.x: trainSet.validation.images,
+                                                       self.y: trainSet.validation.labels, self.LaplaceNoise: LapNoise_test})
+                print("\tEpoch {0} \t validation accuacy: \t {1}".format(epoch, val_acc))
+                #print(val_acc)
+
+        end_time = timeit.default_timer()
+        print("\nThe finetuning process ran for {0} minutes".format((end_time - start_time) / 60))
+
+if __name__ == "__main__":
+    # mnist examples
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    dpn = dpAutoEncoder(n_in=784, n_out=10, hidden_layers_sizes=[25, 90, 25], epsilon = 8.0, _batch_size = 280, finetuneLR = 1e-3)
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    # set random_seed
+    tf.set_random_seed(seed=1111)
+    dpn.pretrain(sess, X_train=mnist)
+    dpn.finetuning(sess, _epsilon = dpn.epsilon, _batch_size = dpn.batch_size, trainSet=mnist)
